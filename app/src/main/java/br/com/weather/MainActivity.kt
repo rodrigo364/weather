2cfc5ce7f55aa8964776f2a827d523f536972fce
Rodrigo Fonseca <digaorfl@hotmail.com>
2022-12-28 17:23:13 -0300

    build: init commit

---
+++ app/src/main/java/br/com/weather/MainActivity.kt
@@ -0,0 +1,20 @@
+package br.com.weather
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+
+
+class MainActivity : ComponentActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text(text = "Ola Tudo joia?")
+        }
+    }
+
+
+}
